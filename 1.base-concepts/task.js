112f5aca586bfb8a39326daf096ef8565bca2732
Mikhail <mfaigenblat@gmail.com>
2022-06-20 16:00:51 +0200

    Initial comit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,24 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b**2-4*a*c;
+
+  if (d === 0) {
+    let x1 = -b/(2*a);
+    arr.push(x1);
+  }
+  else if (d > 0) {
+    let x1 = (-b + Math.sqrt(d))/(2*a); 
+    let x2 = (-b - Math.sqrt(d))/(2*a);
+    arr.push(x1, x2);
+  }
+
+  console.log(arr);
+  return arr; 
 }
+
+solveEquation(1,2,4);
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
