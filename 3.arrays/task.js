f314ef188b949694d26b928f0edb2dfea31ab4ac
Mikhail <mfaigenblat@gmail.com>
2022-06-23 16:24:04 +0200

    arrays homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
 function compareArrays(arr1, arr2) {
-  let result;
 
-  // Ваш код
+  let result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
 
   return result; // boolean
 }
 
-function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+function advancedFilter(arr) {
+
+  let resultArr = arr.filter((n) => n>0).filter((x) => x % 3 === 0).map(i => i*10);
 
   return resultArr; // array
 }
